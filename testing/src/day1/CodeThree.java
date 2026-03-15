ca6ebdbd73b1c5beb0d0363c36c644e9799f2d4e
laxmigupta31111 <laxmi07114@gmail.com>
2026-03-15 17:44:33 +0530

    code3

--- testing/src/day1/CodeThree.java
+++ testing/src/day1/CodeThree.java
@@ -1,5 +1,12 @@
 package day1;
 
 public class CodeThree {
+	int num=7;
+	int sum=0;
+	for (int i = 0; i <=num; i++) {
+		sum=sum+i;
+	}
+	System.out.println("Sum of N natural number is:"+sum);
+}
 
 }
