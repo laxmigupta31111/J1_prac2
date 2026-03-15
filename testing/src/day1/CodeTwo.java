c6bdbe1f0a2cd03e2c5d56bf0166b64642132752
laxmigupta31111 <laxmi07114@gmail.com>
2026-03-15 17:39:15 +0530

    new day1 codes

---
+++ testing/src/day1/CodeTwo.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class CodeTwo {
+
+	int num=5;
+	int fact=1;
+	for (int i = num; i >=1; i--) {
+		fact=fact*i;
+		
+	}
+	
+	System.out.println("Factorial of Number is:"+fact);
+}
+}
